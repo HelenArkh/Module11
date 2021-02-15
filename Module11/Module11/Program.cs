1b491d38c98d3f59f757ad16557b2a2081f9fae6
HelenArkh <alena.arkh@gmail.com>
2021-02-15 19:07:15 +0300

    Создала проект

---
+++ Module11/Module11/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Module11
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+        }
+
+        public void Meth()
+        {
+
+        }
+    }
+}
